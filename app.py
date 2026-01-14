9cc2abdb2ff1b81e46e6d94423e88e48f373b955
Abhinav-droid-sys <abhinav.psingh51@gmail.com>
2026-01-14 21:58:58 +0530

    Update files with login page and auth with Firebase

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from groq import Groq
 import os
 from dotenv import load_dotenv
 from datetime import datetime
+from flask import Flask, render_template
 
 
 # Load environment variables from .env file
 load_dotenv()
 
-app = Flask(__name__, static_folder="static", template_folder="templates")
+app = Flask(__name__, template_folder="templates", static_folder="static")
+
 
 # =========================
 # GROQ CONFIG (SECURE)
@@ -78,6 +80,10 @@ def generate_chat_title(first_message: str) -> str:
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
 @app.route("/privacy")
